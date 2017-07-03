00793f454de1301fd0b9f7fa0066a7cb2a3aafec
tiagolpadua <tiagolpadua@gmail.com>
2017-07-03 10:31:13 -0300

    ex2.1

--- caelumpic/src/app/app.module.ts
+++ caelumpic/src/app/app.module.ts
@@ -1,20 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { FotoModule } from './foto/foto.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FotoModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
