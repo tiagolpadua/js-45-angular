0cfc09fe72fee4ad1c8a8d3d12438c537aaabe8a
tiagolpadua <tiagolpadua@gmail.com>
2017-07-07 21:00:50 -0300

    ex4.7

--- caelumpic/src/app/app.routes.ts
+++ caelumpic/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ListagemComponent } from './listagem/listagem.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 const appRoutes: Routes = [
   { path: '', component: ListagemComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '**', redirectTo: ''}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
