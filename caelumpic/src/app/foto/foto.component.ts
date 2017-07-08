b0077da2ec3aa67fa3da627c429bfb4be0b5941b
tiagolpadua <tiagolpadua@gmail.com>
2017-07-08 17:17:54 -0300

    ex8.7

--- caelumpic/src/app/foto/foto.component.ts
+++ caelumpic/src/app/foto/foto.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
 @Component({
   selector: 'foto',
   templateUrl: './foto.component.html',
-  styleUrls: ['./foto.component.css']
+  styleUrls: ['./foto.component.css'],
+  // encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.Native
 })
+
 export class FotoComponent {
   @Input() titulo: string = '';
   @Input() url: string = '';
   descricao: string = '';
+  _id: string = '';
 }
