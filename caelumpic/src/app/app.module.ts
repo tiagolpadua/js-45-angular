69b5a0f578fc74a24e8b81f6693de962aaa436d7
tiagolpadua <tiagolpadua@gmail.com>
2017-07-08 11:06:26 -0300

    ex6.9

--- caelumpic/src/app/app.module.ts
+++ caelumpic/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import 'rxjs/add/operator/map';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListagemComponent } from './listagem/listagem.component';
-import { routing } from './app.routes';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
+import { routing } from './app.routes';
+// Importa o módulo. Não esqueça de adicioná-lo no array de imports!
+import { FormsModule } from '@angular/forms';
 @NgModule({
   imports: [
-    FotoModule,
-    PainelModule,
-    HttpModule,
     BrowserModule,
-    routing],
-  declarations: [
-    AppComponent,
-    CadastroComponent,
-    ListagemComponent],
+    HttpModule,
+    PainelModule,
+    FotoModule,
+    routing,
+    FormsModule
+  ],
+  declarations: [AppComponent, ListagemComponent, CadastroComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
