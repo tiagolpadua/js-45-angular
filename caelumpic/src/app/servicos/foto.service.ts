797ffb8a1a807e0facc4b873f6f307fa04f9ced5
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-08-19 18:41:30 -0300

    ex 6.6

---
+++ caelumpic/src/app/servicos/foto.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FotoComponent } from '../foto/foto.component';
+export class FotoService {
+    constructor(private http: HttpClient
+        , private cabecalho
+        , private url) {
+        this.cabecalho = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        };
+        this.url = 'http://localhost:3000/v1/fotos/';
+    }
+    listar(): Observable<FotoComponent[]> {
+        return this.http.get<FotoComponent[]>(this.url);
+    }
+}
