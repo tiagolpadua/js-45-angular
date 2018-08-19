1408964d15d5de63eb60cf54e5248f9ad8631f56
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-08-19 15:58:47 -0300

    ex 3.5

--- caelumpic/src/app/app.component.ts
+++ caelumpic/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Caelum Pic';
+  constructor(http: HttpClient) { }
 }
--- caelumpic/src/app/app.module.ts
+++ caelumpic/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FotoModule
   ],
   providers: [],
