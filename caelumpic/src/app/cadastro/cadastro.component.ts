46df52d19a137465e70ffbb71e7bc6aaa93f16d9
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-08-19 17:04:54 -0300

    ex 5.12

--- caelumpic/src/app/cadastro/cadastro.component.ts
+++ caelumpic/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FotoComponent } from '../foto/foto.component';
 @Component({
     selector: 'cadastro',
@@ -6,8 +7,25 @@ import { FotoComponent } from '../foto/foto.component';
 })
 export class CadastroComponent {
     foto = new FotoComponent();
-    
+    http: HttpClient;
+    constructor(http: HttpClient) {
+        this.http = http;
+    }
     salvar() {
-        console.log(this.foto);
+        let cabecalho = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        };
+        this.http
+            .post('http://localhost:3000/v1/fotos'
+                , this.foto
+                , cabecalho
+            )
+            .subscribe(
+                () => {
+                    console.log(`Foto ${this.foto.titulo} salva com sucesso`);
+                    this.foto = new FotoComponent();
+                }
+                , erro => console.log(erro)
+            );
     }
 }
