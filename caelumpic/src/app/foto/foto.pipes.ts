9475fa2de8daf0ba9b8524971e8a0e3f564dac7c
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-08-19 20:18:38 -0300

    ex 11.5

---
+++ caelumpic/src/app/foto/foto.pipes.ts
@@ -0,0 +1,10 @@
+import { Pipe } from '@angular/core';
+@Pipe({
+    name: 'filtroPorTitulo'
+})
+export class FiltroPorTitulo {
+    transform(fotos, digitado) {
+        console.log(fotos);
+        console.log(digitado);
+    }
+}
