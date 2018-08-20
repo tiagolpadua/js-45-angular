c4165914bdddd0bc1e23d17e6ba78e1d272200bc
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-08-19 21:13:24 -0300

    ex 12.4

--- caelumpic/src/app/foto/foto.component.ts
+++ caelumpic/src/app/foto/foto.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
 @Component({
     selector: 'foto',
-    templateUrl: './foto.component.html'
+    templateUrl: './foto.component.html',
+    styleUrls: ['./foto.component.css']
 })
 export class FotoComponent {
     @Input() titulo: string = '';
