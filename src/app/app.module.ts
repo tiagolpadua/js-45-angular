b390f342e2dce4e670dea0040e449bf786695516
Tiago Pádua <tiagolpadua@gmail.com>
2018-12-09 19:09:46 -0200

    ex2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FotoModule } from './foto/foto.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FotoModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/foto/foto.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { FotoComponent } from './foto.component';
+@NgModule({
+  declarations: [FotoComponent],
+  exports: [FotoComponent]
+})
+export class FotoModule {}
