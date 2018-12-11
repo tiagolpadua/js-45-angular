2a8276c6179ba1758a9937f59889fd029e529851
Tiago Pádua <tiagolpadua@gmail.com>
2018-12-10 22:43:29 -0200

    ex4.7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ListagemComponent } from './listagem/listagem.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 const rotasApp: Routes = [
   { path: '', component: ListagemComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '**', redirectTo: '' }
 ];
-
 export const roteamento = RouterModule.forRoot(rotasApp);
