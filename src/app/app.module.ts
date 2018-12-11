76aa0d660516bfb19f3aab837b571521751043d1
Tiago Pádua <tiagolpadua@gmail.com>
2018-12-10 22:33:46 -0200

    ex4.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FotoModule],
+  imports: [BrowserModule, HttpClientModule, FotoModule, PainelModule],
   providers: [],
   bootstrap: [AppComponent]
 })
