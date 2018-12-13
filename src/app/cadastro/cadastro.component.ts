3cb4441101ae8d3e4e079ed89c4be4d3550a1994
Tiago Pádua <tiagolpadua@gmail.com>
2018-12-13 00:00:18 -0200

    ex8.8

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { FotoService } from '../servicos/foto.service';
 import { FotoComponent } from '../foto/foto.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router'; // Importou	Router
 @Component({
   selector: 'cadastro',
   templateUrl: './cadastro.component.html'
 })
 export class CadastroComponent {
   foto = new FotoComponent();
-  constructor(private servico: FotoService, private rota: ActivatedRoute) {
+  constructor(private servico: FotoService, private rota: ActivatedRoute, private roteador: Router) {
+    // Injetou	Router
     this.rota.params.subscribe(parametros => {
       if (parametros.idFoto) {
         this.servico.obterFoto(parametros.idFoto).subscribe(fotoDaApi => (this.foto = fotoDaApi));
@@ -18,9 +19,14 @@ export class CadastroComponent {
 
   salvar() {
     if (this.foto._id) {
-      this.servico
-        .alterar(this.foto)
-        .subscribe(() => console.log(`Foto	${this.foto.titulo}	alterada	com	sucesso`), erro => console.log(erro));
+      this.servico.alterar(this.foto).subscribe(
+        () => {
+          console.log(`Foto	${this.foto.titulo}	alterada	com	sucesso`);
+          // Navegar	após	3	segundos
+          setTimeout(() => this.roteador.navigate(['']), 3000);
+        },
+        erro => console.log(erro)
+      );
     } else {
       this.servico.cadastrar(this.foto).subscribe(
         () => {
