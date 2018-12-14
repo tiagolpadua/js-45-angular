5a339ccd6f00e10f37b881e1ef267f0e5acc9db4
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-12-13 22:41:46 -0200

    ex10.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Caelum	Pic';
+  listaFotos;
+  constructor(http: HttpClient) {
+    http
+      .get('http://localhost:3000/v1/fotos')
+      // .subscribe(fotosApi => (this.listaFotos = fotosApi), erro => console.log(erro));
+      .subscribe(function(retorno) {
+        this.listDaFotos = retorno;
+      });
+  }
+}
