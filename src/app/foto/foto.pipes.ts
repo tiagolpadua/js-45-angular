da17dc400503799655f77dd32b0f3d0caaa99c62
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-12-13 23:06:02 -0200

    ex11.7

--- src/app/foto/foto.pipes.ts
+++ src/app/foto/foto.pipes.ts
@@ -1,10 +1,12 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+
 @Pipe({
   name: 'filtroPorTitulo'
 })
-export class FiltroPorTitulo {
-  transform(fotos, digitado) {
-    console.log(fotos);
-    console.log(digitado);
+export class FiltroPorTitulo implements PipeTransform {
+  // definindo os tipos de todos parâmetros, inclusive o retorno de transform
+  transform(fotos: any, digitado: string): any {
+    console.log(fotos); // quem deve ser filtrado
+    console.log(digitado); // valor usado como filtro
   }
 }
